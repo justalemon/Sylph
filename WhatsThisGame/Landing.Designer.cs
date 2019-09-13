7a2bb14d574bc0e8ac503d4f1fcc2e5fe85c1481
Hannele Ruiz <justlemoncl@gmail.com>
2019-09-13 07:39:43 -0300

    Added some items for showing the already implemented values

--- WhatsThisGame/Landing.Designer.cs
+++ WhatsThisGame/Landing.Designer.cs
@@ -32,7 +32,12 @@ namespace WhatsThisGame
             this.FileGroupBox = new System.Windows.Forms.GroupBox();
             this.FileTextBox = new System.Windows.Forms.TextBox();
             this.FileButton = new System.Windows.Forms.Button();
+            this.BasicGroupBox = new System.Windows.Forms.GroupBox();
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.IdentifierLabel = new System.Windows.Forms.Label();
+            this.ConsoleLabel = new System.Windows.Forms.Label();
             this.FileGroupBox.SuspendLayout();
+            this.BasicGroupBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // FileGroupBox
@@ -68,17 +73,61 @@ namespace WhatsThisGame
             this.FileButton.Text = "Open";
             this.FileButton.UseVisualStyleBackColor = true;
             // 
+            // BasicGroupBox
+            // 
+            this.BasicGroupBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.BasicGroupBox.Controls.Add(this.ConsoleLabel);
+            this.BasicGroupBox.Controls.Add(this.IdentifierLabel);
+            this.BasicGroupBox.Controls.Add(this.NameLabel);
+            this.BasicGroupBox.Location = new System.Drawing.Point(12, 69);
+            this.BasicGroupBox.Name = "BasicGroupBox";
+            this.BasicGroupBox.Size = new System.Drawing.Size(343, 100);
+            this.BasicGroupBox.TabIndex = 1;
+            this.BasicGroupBox.TabStop = false;
+            this.BasicGroupBox.Text = "Basic Information";
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Location = new System.Drawing.Point(6, 22);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(60, 13);
+            this.NameLabel.TabIndex = 0;
+            this.NameLabel.Text = "Name/Title";
+            // 
+            // IdentifierLabel
+            // 
+            this.IdentifierLabel.AutoSize = true;
+            this.IdentifierLabel.Location = new System.Drawing.Point(6, 48);
+            this.IdentifierLabel.Name = "IdentifierLabel";
+            this.IdentifierLabel.Size = new System.Drawing.Size(47, 13);
+            this.IdentifierLabel.TabIndex = 4;
+            this.IdentifierLabel.Text = "Identifier";
+            // 
+            // ConsoleLabel
+            // 
+            this.ConsoleLabel.AutoSize = true;
+            this.ConsoleLabel.Location = new System.Drawing.Point(6, 74);
+            this.ConsoleLabel.Name = "ConsoleLabel";
+            this.ConsoleLabel.Size = new System.Drawing.Size(45, 13);
+            this.ConsoleLabel.TabIndex = 5;
+            this.ConsoleLabel.Text = "Console";
+            // 
             // Landing
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(367, 387);
+            this.Controls.Add(this.BasicGroupBox);
             this.Controls.Add(this.FileGroupBox);
             this.Name = "Landing";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "WhatsThisGame";
             this.FileGroupBox.ResumeLayout(false);
             this.FileGroupBox.PerformLayout();
+            this.BasicGroupBox.ResumeLayout(false);
+            this.BasicGroupBox.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -89,6 +138,10 @@ namespace WhatsThisGame
         private System.Windows.Forms.GroupBox FileGroupBox;
         private System.Windows.Forms.Button FileButton;
         private System.Windows.Forms.TextBox FileTextBox;
+        private System.Windows.Forms.GroupBox BasicGroupBox;
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.Label ConsoleLabel;
+        private System.Windows.Forms.Label IdentifierLabel;
     }
 }
 
