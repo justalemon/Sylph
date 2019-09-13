4f0e6bc761308356574967bc93f3495d9e3bf02a
Hannele Ruiz <justlemoncl@gmail.com>
2019-09-13 04:45:52 -0300

    Renamed form to Landing

--- WhatsThisGame/Landing.Designer.cs
+++ WhatsThisGame/Landing.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WhatsThisGame
 {
-    partial class Form1
+    partial class Landing
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Landing
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "Landing";
+            this.Text = "WhatsThisGame";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- WhatsThisGame/Landing.cs
+++ WhatsThisGame/Landing.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace WhatsThisGame
 {
-    public partial class Form1 : Form
+    public partial class Landing : Form
     {
-        public Form1()
+        public Landing()
         {
             InitializeComponent();
         }
