bcb7d072d6a8adcac7c5a9bec0032533feb48238
Hannele Ruiz <justlemoncl@gmail.com>
2019-09-16 08:12:43 -0300

    Added class for storing the information of a region

---
+++ WhatsThisGame/Regions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatsThisGame
+{
+    /// <summary>
+    /// A class for saving the information of a Region.
+    /// </summary>
+    public class RegionSet
+    {
+        /// <summary>
+        /// The readable name of the Region.
+        /// </summary>
+        public string Name { get; private set; }
+        /// <summary>
+        /// The identifier for the region shown at the end of the Cartridge ID.
+        /// </summary>
+        public string Identifier { get; private set; }
+
+        public RegionSet(string name, string identifier)
+        {
+            Name = name;
+            Identifier = identifier;
+        }
+    }
+}
