89c7941b6371e0f6af2f03e7a67a057cbd7ecc6e
Hannele Ruiz <justlemoncl@gmail.com>
2019-09-16 04:11:50 -0300

    Improved the Nintendo DS code by using a BinaryReader

--- WhatsThisGame/Formats/NintendoDS.cs
+++ WhatsThisGame/Formats/NintendoDS.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -12,162 +12,147 @@ namespace WhatsThisGame.Formats
     /// </summary>
     public class NintendoDS : Format
     {
+        /// <summary>
+        /// The possible console values of a cart.
+        /// </summary>
+        private static readonly Dictionary<byte, string> Consoles = new Dictionary<byte, string>
+        {
+            { 0, "Nintendo DS" },
+            { 2, "Nintendo DS (DSi-enhanced)" },
+            { 3, "Nintendo DSi" },
+        };
         public NintendoDS(Stream stream) : base(stream)
         {
-            // Let's grab the title!
-            // Go to the start of the stream
+            // For safety reasons, go to the start of the stream
             stream.Position = 0;
-            // Grab the first 12 bytes
-            byte[] TitleBytes = new byte[12];
-            stream.Read(TitleBytes, 0, 12);
-            // And save them
-            Title = Encoding.UTF8.GetString(TitleBytes).Trim();
 
-            // Let's finish with the console for the game
-            // Return to the start of the file
-            stream.Position = 0x012;
-            // Check what console corresponds to the ID
-            switch (stream.ReadByte())
+            // Open a BinaryReader
+            using (BinaryReader reader = new BinaryReader(stream))
             {
-                // For zero, is NDS only
-                case 0:
-                    Console = "Nintendo DS";
-                    break;
-                // For two, is NDS + DSi enhanced
-                case 2:
-                    Console = "Nintendo DS (DSi-enhanced)";
-                    break;
-                // For three, is a DSi exclusive
-                case 3:
-                    Console = "Nintendo DSi";
-                    break;
-            }
+                // Get the characters of the title
+                Title = new string(reader.ReadChars(12)).Trim();
+                // Then, get the console for the game
+                reader.BaseStream.Position = 0x012;
+                byte ConsoleID = reader.ReadByte();
+                Console = Consoles.ContainsKey(ConsoleID) ? Consoles[ConsoleID] : $"Unknown (ID: {ConsoleID})";
 
-            // Let's continue with the Cartdige Identifier
-            // Go to the respective position
-            stream.Position = 0xC;
-            // Grab the 4 bytes
-            byte[] IDBytes = new byte[4];
-            stream.Read(IDBytes, 0, 4);
-            // If the 4 bytes are zeroes, this is homebrew so return an empty string
-            if (IDBytes == new byte[4] { 0, 0, 0, 0 })
-            {
-                Identifier = "";
-            }
-            // Otherwise, save the identifier with NTR (DS) or TWL (DSI) appended
-            else
-            {
+                // Now is time for the cartdige identifier (also known as game code)
+                // Set the position to the 4 characters code
+                reader.BaseStream.Position = 0xC;
+                // Read the characters and trim the whitespaces
+                string Code = new string(reader.ReadChars(4)).Trim();
+                // To create the real code, we need to get the destination console for the game
                 string BaseID = Console == "Nintendo DSi" ? "TWL-" : "NTR-";
-                Identifier = BaseID + Encoding.UTF8.GetString(IDBytes).Trim();
-            }
+                Identifier = string.IsNullOrWhiteSpace(Code) ? string.Empty : BaseID + Code;
 
-            // Finally, proceed to check the region
-            // The following regions do not exist: B and G
-            char Character = Identifier[Identifier.Length - 1];
-            switch (Character)
-            {
-                case 'A':
-                    Region = "Asia";
-                    Identifier += "-ASI";
-                    break;
-                case 'C':
-                    Region = "China";
-                    Identifier += "-CHN";
-                    break;
-                case 'D':
-                    if (Identifier == "NTR-BQZD" || Identifier == "NTR-BMYD")
-                    {
-                        Region = "Germany";
-                        Identifier += "-GER";
-                    }
-                    else
-                    {
+                // Finally, proceed to check the region
+                // The following regions do not exist: B and G
+                char Character = Identifier[Identifier.Length - 1];
+                switch (Character)
+                {
+                    case 'A':
+                        Region = "Asia";
+                        Identifier += "-ASI";
+                        break;
+                    case 'C':
+                        Region = "China";
+                        Identifier += "-CHN";
+                        break;
+                    case 'D':
+                        if (Identifier == "NTR-BQZD" || Identifier == "NTR-BMYD")
+                        {
+                            Region = "Germany";
+                            Identifier += "-GER";
+                        }
+                        else
+                        {
+                            Region = "Europe";
+                            Identifier += "-NOE";
+                        }
+                        break;
+                    case 'E':
+                        Region = "United States of America";
+                        Identifier += "-USA";
+                        break;
+                    case 'F':
+                        Region = "France";
+                        Identifier += "-FRA";
+                        break;
+                    case 'H':
+                        Region = "Holland/Netherlands";
+                        Identifier += "-HOL";
+                        break;
+                    case 'I':
+                        Region = "Italy";
+                        Identifier += "-ITA";
+                        break;
+                    case 'J':
+                        Region = "Japan";
+                        Identifier += "-JPN";
+                        break;
+                    case 'K':
+                        Region = "South Korea";
+                        Identifier += "-KOR";
+                        break;
+                    case 'L':
+                        Region = "United States of America";
+                        Identifier += "-USA";
+                        break;
+                    case 'M':
+                        Region = "Sweeden";
+                        Identifier += "-SWE";
+                        break;
+                    case 'N':
+                        Region = "Norway";
+                        Identifier += "-NOR";
+                        break;
+                    case 'O':
+                        Region = "International";
+                        Identifier += "-INT";
+                        break;
+                    case 'P':
                         Region = "Europe";
-                        Identifier += "-NOE";
-                    }
-                    break;
-                case 'E':
-                    Region = "United States of America";
-                    Identifier += "-USA";
-                    break;
-                case 'F':
-                    Region = "France";
-                    Identifier += "-FRA";
-                    break;
-                case 'H':
-                    Region = "Holland/Netherlands";
-                    Identifier += "-HOL";
-                    break;
-                case 'I':
-                    Region = "Italy";
-                    Identifier += "-ITA";
-                    break;
-                case 'J':
-                    Region = "Japan";
-                    Identifier += "-JPN";
-                    break;
-                case 'K':
-                    Region = "South Korea";
-                    Identifier += "-KOR";
-                    break;
-                case 'L':
-                    Region = "United States of America";
-                    Identifier += "-USA";
-                    break;
-                case 'M':
-                    Region = "Sweeden";
-                    Identifier += "-SWE";
-                    break;
-                case 'N':
-                    Region = "Norway";
-                    Identifier += "-NOR";
-                    break;
-                case 'O':
-                    Region = "International";
-                    Identifier += "-INT";
-                    break;
-                case 'P':
-                    Region = "Europe";
-                    Identifier += "-EUR";
-                    break;
-                case 'Q':
-                    Region = "Denmark";
-                    Identifier += "-DEN";
-                    break;
-                case 'R':
-                    Region = "Russia";
-                    Identifier += "-RUS";
-                    break;
-                case 'S':
-                    Region = "Spain/Latin America";
-                    Identifier += "-SPA";
-                    break;
-                case 'U':
-                    Region = "Australia";
-                    Identifier += "-AUS";
-                    break;
-                case 'V':
-                    if (Identifier.StartsWith("NTR-"))
-                    {
-                        Region = "United Kingdom";
-                        Identifier += "-UKV";
-                    }
-                    else
-                    {
-                        Region = "Europe/Schengen Area";
-                        Identifier += "-EUR/EUU";
-                    }
-                    break;
-                case 'W':
-                case 'X':
-                case 'Y':
-                case 'Z':
-                    Region = "Europe";
-                    Identifier += "-EUU";
-                    break;
-                default:
-                    Region = "Unknown";
-                    break;
+                        Identifier += "-EUR";
+                        break;
+                    case 'Q':
+                        Region = "Denmark";
+                        Identifier += "-DEN";
+                        break;
+                    case 'R':
+                        Region = "Russia";
+                        Identifier += "-RUS";
+                        break;
+                    case 'S':
+                        Region = "Spain/Latin America";
+                        Identifier += "-SPA";
+                        break;
+                    case 'U':
+                        Region = "Australia";
+                        Identifier += "-AUS";
+                        break;
+                    case 'V':
+                        if (Identifier.StartsWith("NTR-"))
+                        {
+                            Region = "United Kingdom";
+                            Identifier += "-UKV";
+                        }
+                        else
+                        {
+                            Region = "Europe/Schengen Area";
+                            Identifier += "-EUR/EUU";
+                        }
+                        break;
+                    case 'W':
+                    case 'X':
+                    case 'Y':
+                    case 'Z':
+                        Region = "Europe";
+                        Identifier += "-EUU";
+                        break;
+                    default:
+                        Region = "Unknown";
+                        break;
+                }
             }
         }
 
