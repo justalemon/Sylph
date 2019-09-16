fe589e32574a483cdbeae2303eb7b79d749e9d87
Hannele Ruiz <justlemoncl@gmail.com>
2019-09-16 04:20:54 -0300

    Added support for fetching the Nintendo DS/DSi developer name

--- WhatsThisGame/Formats/NintendoDS.cs
+++ WhatsThisGame/Formats/NintendoDS.cs
@@ -21,6 +21,95 @@ namespace WhatsThisGame.Formats
             { 2, "Nintendo DS (DSi-enhanced)" },
             { 3, "Nintendo DSi" },
         };
+        /// <summary>
+        /// The list of Nintendo DS/DSi developers.
+        /// </summary>
+        private static readonly Dictionary<string, string> Developers = new Dictionary<string, string>
+        {
+            { "01", "Nintendo" },
+            { "02", "Rocket Games" },
+            { "03", "Imagineer Zoom" },
+            { "04", "Gray Matter" },
+            { "05", "Zamuse" },
+            { "06", "Falcom" },
+            { "07", "Enix" },
+            { "08", "Capcom" },
+            { "09", "Hot B" },
+            { "0A", "Jaleco" },
+            { "13", "Electronic Arts Japan" },
+            { "18", "Hudson Entertainment" },
+            { "20", "Destination Software" },
+            { "36", "Codemasters" },
+            { "41", "Ubisoft" },
+            { "4A", "Gakken" },
+            { "4F", "Eidos" },
+            { "4Q", "Disney Interactive Studios" },
+            { "4Z", "Crave Entertainment" },
+            { "52", "Activision" },
+            { "54", "Rockstar Games" },
+            { "5D", "Midway" },
+            { "5G", "Majesco Entertainment" },
+            { "64", "LucasArts Entertainment" },
+            { "69", "Electronic Arts" },
+            { "6K", "UFO Interactive" },
+            { "6V", "JoWooD Entertainment" },
+            { "70", "Atari" },
+            { "78", "THQ" },
+            { "7D", "Vivendi Universal Games" },
+            { "7J", "Zoo Digital Publishing" },
+            { "7N", "Empire Interactive" },
+            { "7U", "Ignition Entertainment" },
+            { "7V", "Summitsoft Entertainment" },
+            { "8J", "General Entertainment" },
+            { "8P", "SEGA" },
+            { "99", "Rising Star Games" },
+            { "A4", "Konami" },
+            { "AF", "Namco" },
+            { "B2", "Bandai" },
+            { "E9", "Natsume" },
+            { "EB", "Atlus" },
+            { "FH", "Foreign Media Games" },
+            { "FK", "The Game Factory" },
+            { "FP", "Mastiff" },
+            { "FR", "DTP Young" },
+            { "G9", "D3Publisher" },
+            { "GD", "SQUARE ENIX" },
+            { "GL", "Gameloft" },
+            { "GN", "Oxygen Interactive" },
+            { "GR", "GSP" },
+            { "GT", "505 Games" },
+            { "GQ", "Engine Software" },
+            { "GY", "The Game Factory" },
+            { "H3", "Zen" },
+            { "H4", "SNK PLAYMORE" },
+            { "H6", "MYCOM" },
+            { "HC", "Plato" },
+            { "HF", "Level 5" },
+            { "HG", "Graffiti Entertainment" },
+            { "HM", "HMH INTERACTIVE" },
+            { "HV", "BHV Software" },
+            { "LR", "Asylum Entertainment" },
+            { "KJ", "Gamebridge" },
+            { "KM", "Deep Silver" },
+            { "MJ", "MumboJumbo" },
+            { "MT", "Blast Entertainment" },
+            { "NK", "Neko Entertainment" },
+            { "NP", "Nobilis Publishing" },
+            { "PG", "Phoenix Games" },
+            { "PL", "Playlogic" },
+            { "SU", "Slitherine Software" },
+            { "SV", "SevenOne Intermedia" },
+            { "RM", "Rondomedia" },
+            { "RT", "RTL Games" },
+            { "TK", "Tasuke" },
+            { "TR", "Tetris Online" },
+            { "TV", "Tivola Publishing" },
+            { "VP", "Virgin Play" },
+            { "WP", "White Park Bay" },
+            { "WR", "Warner Bros" },
+            { "XS", "Aksys Games" },
+        };
+
         public NintendoDS(Stream stream) : base(stream)
         {
             // For safety reasons, go to the start of the stream
@@ -44,6 +133,14 @@ namespace WhatsThisGame.Formats
                 // To create the real code, we need to get the destination console for the game
                 string BaseID = Console == "Nintendo DSi" ? "TWL-" : "NTR-";
                 Identifier = string.IsNullOrWhiteSpace(Code) ? string.Empty : BaseID + Code;
+
+                // Time for the developer name!
+                // Set the position to the 4 characters code
+                reader.BaseStream.Position = 0x10;
+                // And read the two characters as a string
+                string DevCode = new string(reader.ReadChars(2));
+                // Now, set the developer name
+                Developer = Developers.ContainsKey(DevCode) ? Developers[DevCode] : $"Unknown (Makercode: {DevCode})";
 
                 // Finally, proceed to check the region
                 // The following regions do not exist: B and G
