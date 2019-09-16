993efcb2147704132c6419abfa0a8dfb5d2e8bd5
Hannele Ruiz <justlemoncl@gmail.com>
2019-09-16 07:07:30 -0300

    Added little extension to sanitize Nintendo DS/DSi titles

---
+++ WhatsThisGame/Extensions/String.cs
@@ -0,0 +1,10 @@
+﻿namespace WhatsThisGame.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string Sanitize(this string _string)
+        {
+            return _string.Replace("\0", "").Trim();
+        }
+    }
+}
