ea25b063208d7c730fe8d8725c3b6a0bfdee2518
Hannele Ruiz <justlemoncl@gmail.com>
2019-09-17 06:24:46 -0300

    Added documentation about the existing string extensions

--- Undine.Lib/Extensions/String.cs
+++ Undine.Lib/Extensions/String.cs
@@ -1,7 +1,14 @@
 ﻿namespace Undine.Extensions
 {
+    /// <summary>
+    /// Extensions to be used on your normal every day string's.
+    /// </summary>
     public static class StringExtensions
     {
+        /// <summary>
+        /// Removes the trailing characters out of a string.
+        /// </summary>
+        /// <returns>A System.String without trailing whitespaces or \0.</returns>
         public static string Sanitize(this string _string)
         {
             return _string.Replace("\0", "").Trim();
