f83103f66c5ec3fad78591841727f18da9482f20
Hannele Ruiz <justlemoncl@gmail.com>
2019-09-20 11:38:25 -0300

    Added basic project for the command line interface

---
+++ Undine.CommandLine/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Undine.CommandLine
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            return 0;
+        }
+    }
+}
