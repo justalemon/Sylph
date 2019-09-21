6fb375c555a7b2201d09f0be4da8ad85c560e03b
Hannele Ruiz <justlemoncl@gmail.com>
2019-09-21 16:04:09 -0300

    Added basic printing of the rom information

--- Undine.CommandLine/Program.cs
+++ Undine.CommandLine/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using Undine.Formats;
 
 namespace Undine.CommandLine
@@ -22,6 +23,37 @@ namespace Undine.CommandLine
             if (Parameters == null)
             {
                 return 2;
+            }
+
+            // If the file that got passed does not exists
+            if (!File.Exists(Parameters.File))
+            {
+                return 3;
+            }
+
+            // Open the file specified by the user
+            using (Stream stream = File.Open(Parameters.File, FileMode.Open))
+            using (BinaryReader reader = new BinaryReader(stream))
+            {
+                // Get the format for this rom
+                Format format = Detection.Detect(reader);
+                // If format is null, return with a code 4
+                if (format == null)
+                {
+                    return 4;
+                }
+
+                // Now, time for printing the rom information
+                // If the user doesn't want the complete info
+                if (!Parameters.Complete)
+                {
+                    // Print just some of them
+                    Console.WriteLine($"Title: {format.Title}");
+                    Console.WriteLine($"Identifier: {format.Identifier}");
+                    Console.WriteLine($"Developer: {format.Developer}");
+                    Console.WriteLine($"Console: {format.Console}");
+                    Console.WriteLine($"Region: {format.Region}");
+                }
             }
 
             // If we got here, everything is ok so return with a code zero
