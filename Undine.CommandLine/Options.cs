51a6fd8201b3716c60b7e998d18dd176dcf4a928
Hannele Ruiz <justlemoncl@gmail.com>
2019-09-21 18:17:01 -0300

    Fixed flag for getting the complete information not working

--- Undine.CommandLine/Options.cs
+++ Undine.CommandLine/Options.cs
@@ -6,7 +6,7 @@ namespace Undine.CommandLine
     {
         [Option('f', "file", Required = true, HelpText = "The file to get the information from.")]
         public string File { get; set; }
-        [Option('c', "complete", Required = false, HelpText = "If the a verbose version of the rom info should be printed instead.", Default = false)]
+        [Option('c', "complete", Required = false, HelpText = "If the a verbose version of the rom info should be printed instead.")]
         public bool Complete { get; set; }
     }
 }
