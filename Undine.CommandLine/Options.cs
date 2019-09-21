34d2db2ca7d8ef676fc4facab2bda90778ec25ca
Hannele Ruiz <justlemoncl@gmail.com>
2019-09-21 15:44:58 -0300

    Added support for parsing the command line arguments

---
+++ Undine.CommandLine/Options.cs
@@ -0,0 +1,12 @@
+﻿using CommandLine;
+
+namespace Undine.CommandLine
+{
+    public class Options
+    {
+        [Option('f', "file", Required = true, HelpText = "The file to get the information from.")]
+        public string File { get; set; }
+        [Option('c', "complete", Required = false, HelpText = "If the a verbose version of the rom info should be printed instead.", Default = false)]
+        public bool Complete { get; set; }
+    }
+}
--- Undine.CommandLine/Program.cs
+++ Undine.CommandLine/Program.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using CommandLine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Undine.Formats;
 
 namespace Undine.CommandLine
 {
     public class Program
     {
+        /// <summary>
+        /// The launch parameters sent via the command line.
+        /// </summary>
+        private static Options Parameters { get; set; }
+
         public static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Try to parse the launch parameters
+            Parser.Default.ParseArguments<Options>(args).WithParsed(opts => Parameters = opts);
+
+            // If there is no parameters to use, exit with a code 2
+            if (Parameters == null)
+            {
+                return 2;
+            }
+
+            // If we got here, everything is ok so return with a code zero
             return 0;
         }
     }
